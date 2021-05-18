4c513446e08c22a82df39f9f86d469fb053d0393
Adriano Soares Costa <spike.summer@hotmail.com>
2021-05-18 00:55:16 -0300

    Update index.tsx

--- curriculo-vitae/src/components/Profile/index.tsx
+++ curriculo-vitae/src/components/Profile/index.tsx
@@ -5,7 +5,7 @@ const Profile: React.FC = () => {
     return(
         <Container>
             <Title2>Perfil</Title2>
-            <p>Apesar de apaixonado por tecnologia, me dediquei, na última década, à advocacia, porém, pretendo mudar meu rumo e trilhar um novo caminho. Sou autodidata, extrovertido, alegre e sempre curioso por descobrir coisas novas. Estudar é meu passatempo preferido e o mundo é um livro que pretendo ler. 
+            <p>Apesar de apaixonado por tecnologia, dediquei-me, na última década, à advocacia, porém, pretendo mudar meu rumo e trilhar um novo caminho. Sou autodidata, extrovertido, alegre e sempre curioso por descobrir coisas novas. Estudar é meu passatempo preferido e o mundo é um livro que pretendo ler. 
             </p>
         </Container>
     )
