180f0db117bf0bbad0bc12e0e61adcda6c735e09
Adriano Soares Costa <spike.summer@hotmail.com>
2021-05-17 23:16:07 -0300

    Update index.tsx

--- curriculo-vitae/src/components/Profile/index.tsx
+++ curriculo-vitae/src/components/Profile/index.tsx
@@ -5,7 +5,7 @@ const Profile: React.FC = () => {
     return(
         <Container>
             <Title2>Perfil</Title2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            <p>Apesar de apaixonado por tecnologia, me dediquei na ultima década a advocacia, porém pretendo mudar meu rumo e trilhar um novo caminho. Sou autodidata, extrovertido, alegre e sempre curioso por descobrir coisas novas. Estudar e meu passa tempo preferido e o mundo e um livro que pretendo ler. 
             </p>
         </Container>
     )
